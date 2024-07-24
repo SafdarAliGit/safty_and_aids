0ad884c2529258badf7f623965f318339c2e47a9
safdar211ali <safdar211@hotmail.com>
2024-07-24 18:40:14 +0500

    updated

--- safty_and_aids/public/maintenance_visit.js
+++ safty_and_aids/public/maintenance_visit.js
@@ -1,62 +1,61 @@
 frappe.ui.form.on('Maintenance Visit', {
     refresh(frm) {
-        alert(frm.doc.purposes.length);
-        if (frm.doc.purposes.length == 0) {
-            // Hide fields when purposes are empty
-            frm.fields_dict['custom_clean_agent'].df.hidden = true;
-            frm.fields_dict['custom_application'].df.hidden = true;
-            frm.fields_dict['custom_type_of_detection'].df.hidden = true;
-            frm.fields_dict['custom_type_of_operation'].df.hidden = true;
-            frm.fields_dict['custom_water_supply_source'].df.hidden = true;
-            frm.fields_dict['custom_water_tank'].df.hidden = true;
-            frm.fields_dict['custom_type_of_water'].df.hidden = true;
-            frm.fields_dict['custom_location_area_unit'].df.hidden = true;
-            frm.fields_dict['custom_main_power_supply'].df.hidden = true;
-            frm.fields_dict['custom_back_up_power_supply'].df.hidden = true;
-            frm.fields_dict['custom_type_of_circuit_wiring'].df.hidden = true;
-        } else {
-            // Show/Hide fields based on the item_code
-            if (frm.doc.purposes[0].item_code == 'Fire Suppression System') {
-                frm.fields_dict['custom_clean_agent'].df.hidden = false;
-                frm.fields_dict['custom_application'].df.hidden = false;
-                frm.fields_dict['custom_type_of_detection'].df.hidden = false;
-                frm.fields_dict['custom_location_area_unit'].df.hidden = false;
-                frm.fields_dict['custom_main_power_supply'].df.hidden = false;
-                frm.fields_dict['custom_back_up_power_supply'].df.hidden = false;
-                frm.fields_dict['custom_type_of_circuit_wiring'].df.hidden = false;
-
-                frm.fields_dict['custom_type_of_operation'].df.hidden = true;
-                frm.fields_dict['custom_water_supply_source'].df.hidden = true;
-                frm.fields_dict['custom_water_tank'].df.hidden = true;
-                frm.fields_dict['custom_type_of_water'].df.hidden = true;
-            } else {
-                frm.fields_dict['custom_clean_agent'].df.hidden = true;
-                frm.fields_dict['custom_application'].df.hidden = true;
-                frm.fields_dict['custom_type_of_detection'].df.hidden = true;
-                frm.fields_dict['custom_location_area_unit'].df.hidden = true;
-                frm.fields_dict['custom_main_power_supply'].df.hidden = true;
-                frm.fields_dict['custom_back_up_power_supply'].df.hidden = true;
-                frm.fields_dict['custom_type_of_circuit_wiring'].df.hidden = true;
-
-                frm.fields_dict['custom_type_of_operation'].df.hidden = false;
-                frm.fields_dict['custom_water_supply_source'].df.hidden = false;
-                frm.fields_dict['custom_water_tank'].df.hidden = false;
-                frm.fields_dict['custom_type_of_water'].df.hidden = false;
-            }
-        }
-
-        // Refresh the fields to ensure visibility changes take effect
-        frm.refresh_field('custom_clean_agent');
-        frm.refresh_field('custom_application');
-        frm.refresh_field('custom_type_of_detection');
-        frm.refresh_field('custom_type_of_operation');
-        frm.refresh_field('custom_water_supply_source');
-        frm.refresh_field('custom_water_tank');
-        frm.refresh_field('custom_type_of_water');
-        frm.refresh_field('custom_location_area_unit');
-        frm.refresh_field('custom_main_power_supply');
-        frm.refresh_field('custom_back_up_power_supply');
-        frm.refresh_field('custom_type_of_circuit_wiring');
+        // if (frm.doc.purposes.length == 0) {
+        //     // Hide fields when purposes are empty
+        //     frm.fields_dict['custom_clean_agent'].df.hidden = true;
+        //     frm.fields_dict['custom_application'].df.hidden = true;
+        //     frm.fields_dict['custom_type_of_detection'].df.hidden = true;
+        //     frm.fields_dict['custom_type_of_operation'].df.hidden = true;
+        //     frm.fields_dict['custom_water_supply_source'].df.hidden = true;
+        //     frm.fields_dict['custom_water_tank'].df.hidden = true;
+        //     frm.fields_dict['custom_type_of_water'].df.hidden = true;
+        //     frm.fields_dict['custom_location_area_unit'].df.hidden = true;
+        //     frm.fields_dict['custom_main_power_supply'].df.hidden = true;
+        //     frm.fields_dict['custom_back_up_power_supply'].df.hidden = true;
+        //     frm.fields_dict['custom_type_of_circuit_wiring'].df.hidden = true;
+        // } else {
+        //     // Show/Hide fields based on the item_code
+        //     if (frm.doc.purposes[0].item_code == 'Fire Suppression System') {
+        //         frm.fields_dict['custom_clean_agent'].df.hidden = false;
+        //         frm.fields_dict['custom_application'].df.hidden = false;
+        //         frm.fields_dict['custom_type_of_detection'].df.hidden = false;
+        //         frm.fields_dict['custom_location_area_unit'].df.hidden = false;
+        //         frm.fields_dict['custom_main_power_supply'].df.hidden = false;
+        //         frm.fields_dict['custom_back_up_power_supply'].df.hidden = false;
+        //         frm.fields_dict['custom_type_of_circuit_wiring'].df.hidden = false;
+        //
+        //         frm.fields_dict['custom_type_of_operation'].df.hidden = true;
+        //         frm.fields_dict['custom_water_supply_source'].df.hidden = true;
+        //         frm.fields_dict['custom_water_tank'].df.hidden = true;
+        //         frm.fields_dict['custom_type_of_water'].df.hidden = true;
+        //     } else {
+        //         frm.fields_dict['custom_clean_agent'].df.hidden = true;
+        //         frm.fields_dict['custom_application'].df.hidden = true;
+        //         frm.fields_dict['custom_type_of_detection'].df.hidden = true;
+        //         frm.fields_dict['custom_location_area_unit'].df.hidden = true;
+        //         frm.fields_dict['custom_main_power_supply'].df.hidden = true;
+        //         frm.fields_dict['custom_back_up_power_supply'].df.hidden = true;
+        //         frm.fields_dict['custom_type_of_circuit_wiring'].df.hidden = true;
+        //
+        //         frm.fields_dict['custom_type_of_operation'].df.hidden = false;
+        //         frm.fields_dict['custom_water_supply_source'].df.hidden = false;
+        //         frm.fields_dict['custom_water_tank'].df.hidden = false;
+        //         frm.fields_dict['custom_type_of_water'].df.hidden = false;
+        //     }
+        // }
+        //
+        // // Refresh the fields to ensure visibility changes take effect
+        // frm.refresh_field('custom_clean_agent');
+        // frm.refresh_field('custom_application');
+        // frm.refresh_field('custom_type_of_detection');
+        // frm.refresh_field('custom_type_of_operation');
+        // frm.refresh_field('custom_water_supply_source');
+        // frm.refresh_field('custom_water_tank');
+        // frm.refresh_field('custom_type_of_water');
+        // frm.refresh_field('custom_location_area_unit');
+        // frm.refresh_field('custom_main_power_supply');
+        // frm.refresh_field('custom_back_up_power_supply');
+        // frm.refresh_field('custom_type_of_circuit_wiring');
     }
 });
 
