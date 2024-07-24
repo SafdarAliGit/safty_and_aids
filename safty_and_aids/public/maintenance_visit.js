f749d5cf10c4fd50e02a61d77a09fc03d4404d29
safdar211ali <safdar211@hotmail.com>
2024-07-24 18:42:21 +0500

    updated

--- safty_and_aids/public/maintenance_visit.js
+++ safty_and_aids/public/maintenance_visit.js
@@ -59,72 +59,7 @@ frappe.ui.form.on('Maintenance Visit', {
     }
 });
 
-frappe.ui.form.on('Maintenance Visit', {
-    refresh: function(frm) {
-        // Hide all fields initially
-        const hideFields = [
-            'custom_clean_agent', 'custom_application', 'custom_type_of_detection',
-            'custom_type_of_operation', 'custom_water_supply_source', 'custom_water_tank',
-            'custom_type_of_water', 'custom_location_area_unit', 'custom_main_power_supply',
-            'custom_back_up_power_supply', 'custom_type_of_circuit_wiring'
-        ];
 
-        hideFields.forEach(field => {
-            frm.fields_dict[field].df.hidden = true;
-        });
-
-        frm.refresh_fields(hideFields);
-
-        if (frm.doc.purposes && frm.doc.purposes.length > 0) {
-            const itemCode = frm.doc.purposes[0].item_code;
-
-            if (itemCode === 'Fire Suppression System') {
-                // Show fields specific to 'Fire Suppression System'
-                const showFieldsForFireSuppression = [
-                    'custom_clean_agent', 'custom_application', 'custom_type_of_detection',
-                    'custom_location_area_unit', 'custom_main_power_supply',
-                    'custom_back_up_power_supply', 'custom_type_of_circuit_wiring'
-                ];
-
-                showFieldsForFireSuppression.forEach(field => {
-                    frm.fields_dict[field].df.hidden = false;
-                });
-
-                const hideFieldsForFireSuppression = [
-                    'custom_type_of_operation', 'custom_water_supply_source',
-                    'custom_water_tank', 'custom_type_of_water'
-                ];
-
-                hideFieldsForFireSuppression.forEach(field => {
-                    frm.fields_dict[field].df.hidden = true;
-                });
-            } else {
-                // Show fields specific to other purposes
-                const showFieldsForOtherPurposes = [
-                    'custom_type_of_operation', 'custom_water_supply_source',
-                    'custom_water_tank', 'custom_type_of_water'
-                ];
-
-                showFieldsForOtherPurposes.forEach(field => {
-                    frm.fields_dict[field].df.hidden = false;
-                });
-
-                const hideFieldsForOtherPurposes = [
-                    'custom_clean_agent', 'custom_application', 'custom_type_of_detection',
-                    'custom_location_area_unit', 'custom_main_power_supply',
-                    'custom_back_up_power_supply', 'custom_type_of_circuit_wiring'
-                ];
-
-                hideFieldsForOtherPurposes.forEach(field => {
-                    frm.fields_dict[field].df.hidden = true;
-                });
-            }
-
-            // Refresh fields based on updated visibility
-            frm.refresh_fields([...hideFields, ...showFieldsForFireSuppression, ...showFieldsForOtherPurposes]);
-        }
-    }
-});
 
 frappe.ui.form.on('Maintenance Visit Purpose', {
     item_code: function (frm, cdt, cdn) {
