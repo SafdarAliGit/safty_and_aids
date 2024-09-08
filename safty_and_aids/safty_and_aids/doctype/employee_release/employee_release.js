32432630756db1c834c2bcf94ca2e3c4fd640c75
safdar211ali <safdar211@hotmail.com>
2024-09-08 22:44:42 +0500

    updated

---
+++ safty_and_aids/safty_and_aids/doctype/employee_release/employee_release.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, portal and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Employee Release", {
+// 	refresh(frm) {
+
+// 	},
+// });
