ba58b0ce6335d69b599065537a02e41d4b9f124f
safdar211ali <safdar211@hotmail.com>
2024-12-29 11:42:07 +0500

    updated

--- safty_and_aids/customfilters/utils.py
+++ safty_and_aids/customfilters/utils.py
@@ -5,5 +5,5 @@ def gregorian_to_hijri(date):
     if date:
         g_date = datetime.strptime(date, "%Y-%m-%d")
         hijri_date = Gregorian(g_date.year, g_date.month, g_date.day).to_hijri()
-        return f"{hijri_date.year}/{hijri_date.month}/{hijri_date.day}"
+        return f"{hijri_date.year}-{hijri_date.month}-{hijri_date.day}"
     return ""
