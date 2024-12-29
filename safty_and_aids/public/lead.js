a70665403406e752577da234e3b52248f1279084
safdar211ali <safdar211@hotmail.com>
2024-12-29 14:01:52 +0500

    updated

--- safty_and_aids/public/lead.js
+++ safty_and_aids/public/lead.js
@@ -5,7 +5,7 @@ frappe.ui.form.on('Lead', {
                 'customer': frm.doc.custom_customer,
                 'lead_name': frm.doc.lead_name,
                 'lead_id':frm.doc.name,
-                'phone': frm.doc.mobile_no,
+                'mobile_number': frm.doc.mobile_no,
                 'company': frm.doc.company,
 
             };
