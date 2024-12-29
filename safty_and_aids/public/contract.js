6b48e2f04d880c0a3a7ed65c967b9f32097fbeea
safdar211ali <safdar211@hotmail.com>
2024-12-29 09:50:27 +0500

    updated

--- safty_and_aids/public/contract.js
+++ safty_and_aids/public/contract.js
@@ -1,28 +1,27 @@
 frappe.ui.form.on('Contract', {
-    onload: function(frm) {
+    before_print: function (frm) {
         if (frm.doc.language === "Arabic") {
-            frm.set_value('Contract-Ar PF');
-        } else{
-            frm.set_value('Contract-Eng PF');
+            frm.meta.default_print_format = "Contract-Ar PF";
         }
+
     },
     refresh(frm) {
         // your code here
     },
-    document_name:function (frm){
-            frappe.call({
-                method: 'safty_and_aids.utils.get_total_from_quotation',
-                args: {
-                    id: frm.doc.document_name
-                },
-                callback: function (response) {
-                    if (response.message) {
-                          frm.set_value("custom_total", response.message.total);
-                    } else {
-                        frappe.msgprint(__('Record not found for Quotation: {0}', [frm.doc.document_name]));
-                    }
+    document_name: function (frm) {
+        frappe.call({
+            method: 'safty_and_aids.utils.get_total_from_quotation',
+            args: {
+                id: frm.doc.document_name
+            },
+            callback: function (response) {
+                if (response.message) {
+                    frm.set_value("custom_total", response.message.total);
+                } else {
+                    frappe.msgprint(__('Record not found for Quotation: {0}', [frm.doc.document_name]));
                 }
-            });
+            }
+        });
 
     }
 })
