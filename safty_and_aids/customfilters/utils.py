478ac613828205620a6fdaf759973474fc93a975
safdar211ali <safdar211@hotmail.com>
2024-12-29 11:51:11 +0500

    updated

--- safty_and_aids/customfilters/utils.py
+++ safty_and_aids/customfilters/utils.py
@@ -1,9 +1,20 @@
 from hijri_converter import Gregorian
 from datetime import datetime
 
+from hijri_converter import Gregorian
+from datetime import datetime
+
 def gregorian_to_hijri(date):
     if date:
-        g_date = datetime.strptime(date, "%Y-%m-%d")
+        # Check if the input is a datetime.date object
+        if isinstance(date, datetime):
+            g_date = date
+        elif isinstance(date, str):
+            g_date = datetime.strptime(date, "%Y-%m-%d")
+        else:
+            return ""  # Return an empty string if the input is neither
+
+        # Convert Gregorian date to Hijri
         hijri_date = Gregorian(g_date.year, g_date.month, g_date.day).to_hijri()
         return f"{hijri_date.year}-{hijri_date.month}-{hijri_date.day}"
     return ""
