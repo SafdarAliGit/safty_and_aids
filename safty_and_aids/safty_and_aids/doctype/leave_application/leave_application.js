8ed6e35b8ea5267b8717cc30f63408df75fd71ed
safdar211ali <safdar211@hotmail.com>
2025-01-11 22:33:49 +0500

    updated

--- safty_and_aids/safty_and_aids/doctype/leave_application/leave_application.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, portal and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Leave Application", {
-// 	refresh(frm) {
-
-// 	},
-// });
